8d5e6be246c0f48df9beb7dedfb8edec3149b656
Sam-Abood <Yyyyy66666>
2016-06-29 19:34:37 -0600

    Polishing
    - added fps display functionality
    - tidied up code

--- Client/main.js
+++ Client/main.js
@@ -2,26 +2,22 @@
 var blobSize = 12,	// blob radius on screen
 	blobDensity = 1.0/1700, //  (blobs_areas) / available_area
  	blobFactor = 20, // how much size increases by eating a blob
- 	blobs = [],	// store positions of blobs
- 	sizeup = 0; // extra size gained by eating blobs... to be sent to server!! 
-
-/* Intervals, rates and timings */
-var send_interval = 60,			// sending input to server 
- 	blob_gen_interval = 30000;	    // new blob-set generating
+ 	blobs = [];	// store positions of blobs
 
 /* Other variables */
 var players = [];
 var ip_address = "localhost", port = "8080";
 var colors = ["magenta", "yellow", "purple", "pink", "chartreuse", "orange", "aqua", "bronze", "red"];
-var wrdWidth = 2000*3, wrdHeight = 2000*3;
+var wrdWidth = 2000*3, wrdHeight = 2000*3;				// world dimensions
 var conn;
 var inSeq = 0, inBuff = [];
 var ease_step = 0.45, ease_spd = 10;
-var lastTime, times = 60, ms = 1000.0 / times, delta = 0.0;	
-var xshift, yshift;
+var lastTime, times = 60, ms = 1000.0 / times, delta = 0.0;		// for delta-timing adnd constant framerate
+var xshift, yshift;			// for translating the world to be in main player's perspective
 var batch_size = 30;		// how many user inputs to handle and send each frame  TODO: tweak
+var fps = [], _ind_ = 0;	// for showing fps
 
-/************************************************/
+/*************************************************************************************************************************/
 function Organ(xpos, ypos, size, xSpd, ySpd) {
 	this.lock = false;		// prevent organs from going apart
 	this.x = xpos;
@@ -47,6 +43,7 @@ Organ.prototype.move = function(){
 
 Organ.prototype.update = function () {
 	this.move();
+
 	var dt = 1.0/times;
 	if(this.applyPosEase){	
 		this.x += (ease_spd*dt*ease_step*this.easeDist) * this.easex;
@@ -77,7 +74,6 @@ Organ.prototype.easeSize = function(mass_delta) {
 };
 
 Organ.prototype.split = function() {
-
 	this.easeSize(-this.size/2.0);
 
 	var org2 = new Organ(this.x, this.y, this.size/2,
@@ -106,7 +102,7 @@ function Player(player_id) {
 	this.directY = 0;
 }
 
-Player.prototype.restrict = function(){	// restrict organs movements
+Player.prototype.constrain = function(){	// constrain organs movements
 	// after the organs are packed, they can't keep going in their direction, they have to start going in the CM direction
 	for(var i = 0; i < this.organs.length; i++) {
 		var org = this.organs[i];
@@ -150,21 +146,20 @@ Player.prototype.restrict = function(){	// restrict organs movements
 
 	}
 
-
 };
 
-function calCM (pl) {
+Player.prototype.calCM = function() {
 	var avgX = 0.0, avgY = 0.0;
-	var count = pl.organs.length;
+	var count = this.organs.length;
 
 	for(var i=0; i < count; i++) {
-		avgX += pl.organs[i].x;
-		avgY += pl.organs[i].y;
+		avgX += this.organs[i].x;
+		avgY += this.organs[i].y;
 	}
 
-	pl.cmx = avgX/count;
-	pl.cmy = avgY/count;
-}
+	this.cmx = avgX/count;
+	this.cmy = avgY/count;
+};
 
 /************************************************/
 
@@ -173,9 +168,10 @@ function Point (x,y,col) {
 	this.y = y;
 	this.color = col;
 	this.ang = Math.random()*Math.PI;
+	this.isVirus = false;
 }
 
-//-----------------------------------------------------------------------------------------------------------//
+/*************************************************************************************************************************/
 
 window.onload = function() {
 	var canvas = document.getElementById("canvas"),
@@ -183,30 +179,22 @@ window.onload = function() {
 		width = canvas.width = window.innerWidth,
 		height = canvas.height = window.innerHeight;
 	var ready = false;
-
-	generateBlobs();
-	
 	var mp = new Player(-1);		// main player
 	mp.x = -1,
 	mp.y = -1;
 
+	generateBlobs();
 	Connect();
+	initFPS();
+	var last  = performance.now();
 	run();
 	
-
-
-
-    /*****************  FUNCTIONS  *****************/
 
 function Connect(){
 	conn = new WebSocket('ws://'+ip_address+':'+port);
 	conn.onopen = function(e) {
 		console.log("Connected to server");
-	};
-
-	conn.onclose = function(e) {
-		ready = false;
-	    setTimeout(Connect, 5000);
+		addEventListeners();	
 	};
 
 	var fst_msg = true;
@@ -231,7 +219,8 @@ function Connect(){
 			return;
 		}
 		return;
-		/*// if it's not the first msg:
+
+		/* if it's not the first msg:
 		var e_str = String(e.data);
 		var str_array = e_str.split(';');
 		
@@ -253,179 +242,214 @@ function Connect(){
 		}*/
 
 	};
-}
 
-	document.body.addEventListener("mousemove", function(event) {
-		inBuff.push(
-			{	
+	conn.onclose = function(e) {
+		ready = false;
+	    setTimeout(Connect, 5000);
+	};
+
+	function addEventListeners(){
+		document.body.addEventListener("mousemove", function(event) {
+			inBuff.push(
+				{	
+					seq : inSeq,
+					xdir : event.clientX-(width/2.0),
+					ydir : event.clientY-(height/2.0),
+					inType : "mm"
+				});
+			inSeq++;
+		});
+
+		document.body.addEventListener("mousedown", function(event) {
+			inBuff.push({	
 				seq : inSeq,
 				xdir : event.clientX-(width/2.0),
 				ydir : event.clientY-(height/2.0),
-				inType : "mm"
+				inType : "md"
 			});
-		inSeq++;
-	});
-
-	document.body.addEventListener("mousedown", function(event) {
-		inBuff.push({	
-			seq : inSeq,
-			xdir : event.clientX-(width/2.0),
-			ydir : event.clientY-(height/2.0),
-			inType : "md"
+			inSeq++;
 		});
-		inSeq++;
-	});
+	}
 
+}	// end connect()
 
-	function generateBlobs() {
-		var world_area = wrdWidth*wrdHeight;
-		var available_area = world_area;// - size;
-		/* blob_count * blobSize / available_area = density  */
-		var blob_count = (blobDensity * available_area) / blobSize;
-		//console.log(blob_count);
-		blobs = [];
-		for (var i = 0; i < blob_count; i++) {
-			//var pos_mag = size + 10 - (wrdWidth/2) + Math.random()*wrdWidth ;  
-			//var pos_angle = 2*Math.PI* Math.random();
-			//blobs.push(  new Point(pos_mag*Math.cos(pos_angle), pos_mag*Math.sin(pos_angle), colors[Math.floor((Math.random() * colors.length))])  );
-			blobs.push( new Point(-(wrdWidth/2) + wrdWidth*Math.random(), -(wrdHeight/2) + wrdHeight*Math.random(), colors[Math.floor((Math.random() * colors.length))]) );
+function send(input) {
+	if(input)
+		conn.send(input.inSeq+","+input.inType+","+input.xdir+","+input.ydir);
+}
+
+function run() {		// Main game-loop function
+	if(ready) {
+
+		var batchSize = batch_size;
+		while(batchSize > 0 && inBuff.length>0) {
+		var input = inBuff.shift();			// shift() removes the first element.... as in Queues
+		//send(input);		TODO: enable later
+			process(input);
+			batchSize--;
 		}
-	}
-
-	// draw a polygon with given sides and radius(for the circumscribing circle) 
-	function drawCircle(x,y,rad,sides,col,start) {
-		var ang = 2*Math.PI/sides;
-		var cur = start;
-
-		context.beginPath();
-		context.moveTo(x+ rad*Math.cos(cur), y+rad*Math.sin(cur));
-		for(var i=0; i<sides; i++) {
-			context.lineTo(x+ rad*Math.cos(cur+ang), y+rad*Math.sin(cur+ang));
-			cur += ang;
-		}	
-		context.closePath();
-		context.fillStyle = col;	
-		context.fill();
-	}
-
-
-	function run() {		// Main game-loop function
-		if(ready) {
-			var batchSize = batch_size;
-			while(batchSize > 0 && inBuff.length>0) {
-				var input = inBuff.shift();			// shift() removes the first element.... as in Queues
-				//send(input);		TODO: enable later
-				process(input);
-				batchSize--;
-			}
-
-			var now = performance.now();
-		    delta += (now - lastTime) / ms;  // detla += actual elapsed time / time required for 1 update 
-			lastTime = now;
-
-			if(delta >= 1) { 
-				for(var i=0; i<mp.organs.length; i++) 
-					mp.organs[i].update();
-						
-				mp.restrict();	
-
-				calCM(mp);
-				delta--;
-			}
-
-			context.clearRect(0, 0, width, height);
-			xshift = mp.cmx - width/2;
-			yshift = mp.cmy - height/2; 
-			drawGrid();
-			drawBlobs();
-			for(var i=0; i<mp.organs.length; i++) 
-				mp.organs[i].draw(context);
 		
+		var now = performance.now();
+		displayFPS(now);
+	    delta += (now - lastTime) / ms;  // detla += actual elapsed time / time required for 1 update 
+		lastTime = now;
+
+
+	
+		if(delta >= 1) { 
+			// updates here:   will run at times/second
+			for(var i=0; i<mp.organs.length; i++) 
+				mp.organs[i].update();
+			mp.constrain();	
+			mp.calCM();
+
+			delta--;
 		}
 
-		else{		
-			context.fillStyle = '#333333';
-            context.fillRect(0, 0, width, height);
+		context.clearRect(0, 0, width, height);
+		xshift = mp.cmx - width/2;
+		yshift = mp.cmy - height/2; 
+		drawGrid();
+		drawBlobs();
+		for(var i=0; i<mp.organs.length; i++) 
+			mp.organs[i].draw(context);
 
-            context.textAlign = 'center';
-            context.fillStyle = '#FFFFFF';
-            context.font = 'bold 30px sans-serif';
-			context.fillText('Connecting to server...', width/2, height/2);
-			//if(conn.readState != 1 || conn == null)
-			//	conn = new WebSocket('ws://'+ip_address+':'+port);
-		}
-
-		requestAnimationFrame(run);
 	}
 
-	function send(input) {
-		if(input)
-			conn.send(input.inSeq+","+input.inType+","+input.xdir+","+input.ydir);
+	else {	// if not ready	
+		context.fillStyle = '#333333';
+        context.fillRect(0, 0, width, height);
+
+        context.textAlign = 'center';
+        context.fillStyle = '#FFFFFF';
+        context.font = 'bold 30px sans-serif';
+		context.fillText('Connecting to server...', width/2, height/2);
 	}
 
-	function process(input) {
-		var tempOrgans = [];
-		for(var i=0; i < mp.organs.length; i++) {
-			// each orgnas will try to move towards the mouse pointer, but later when the orgnas are packed together, they'll go in CM direction
-			var xspd = mp.organs[i].xspd;
-			var yspd = mp.organs[i].yspd;
-			var mag = Math.sqrt(xspd*xspd + yspd*yspd);
-			var ang = Math.atan2( (input.ydir+height/2) - (mp.organs[i].y-yshift),			// the direction angle from the organ to the mouse location
-								  (input.xdir+width/2)  - (mp.organs[i].x -xshift)  );
+	requestAnimationFrame(run);
+}	// end run()
 
-			mp.organs[i].xspd = Math.cos(ang) * mag;
-			mp.organs[i].yspd = Math.sin(ang) * mag;
+function process(input) {
+	var tempOrgans = [];
+	for(var i=0; i < mp.organs.length; i++) {
+		// each orgnas will try to move towards the mouse pointer, but later when the orgnas are packed together, they'll follow CM direction
+		var xspd = mp.organs[i].xspd;
+		var yspd = mp.organs[i].yspd;
+		var mag = Math.sqrt(xspd*xspd + yspd*yspd);
+		var ang = Math.atan2( (input.ydir+height/2) - (mp.organs[i].y-yshift),			// the direction angle from the organ to the mouse location
+							  (input.xdir+width/2)  - (mp.organs[i].x -xshift)  );
 
-			mp.directX = input.xdir;
-			mp.directY = input.ydir;
+		mp.organs[i].xspd = Math.cos(ang) * mag;
+		mp.organs[i].yspd = Math.sin(ang) * mag;
 
-			if(input.inType == 'md')
-				tempOrgans.push(mp.organs[i].split());
-		}
+		mp.directX = input.xdir;
+		mp.directY = input.ydir;
 
-		for(var i=0; i < tempOrgans.length; i++) 
-			mp.organs.push(tempOrgans[i]);
+		if(input.inType == 'md')
+			tempOrgans.push(mp.organs[i].split());
 	}
 
-	function drawGrid() {
-		var maxX = wrdWidth/2, maxY = wrdHeight/2;	
-		var ulx = 2 -xshift -maxX,
-			uly = 2 -yshift- maxY,
-			urx = wrdWidth -xshift-maxX,
-			ury = 2 -yshift-maxY,
-			dlx = 2 -xshift-maxX,
-			dly = wrdHeight -yshift-maxY,
-			drx = wrdWidth -xshift-maxX,
-			dry = wrdHeight -yshift-maxY;
-		context.beginPath();
-		context.moveTo(ulx,uly);
-		context.lineTo(dlx,dly);
-		context.stroke();
-		context.beginPath();
-		context.moveTo(ulx,uly);
-		context.lineTo(urx,ury);
-		context.stroke();
-		context.beginPath();
-		context.moveTo(urx,ury);
-		context.lineTo(drx,dry);
-		context.stroke();
-		context.beginPath();
-		context.moveTo(drx,dry);
-		context.lineTo(dlx,dly);
-		context.stroke();
+	for(var i=0; i < tempOrgans.length; i++) 
+		mp.organs.push(tempOrgans[i]);
+}	// end process()
+
+function drawGrid() {
+	var maxX = wrdWidth/2, maxY = wrdHeight/2;	
+	var ulx = 2 -xshift -maxX,			// upleft
+		uly = 2 -yshift- maxY,
+		urx = wrdWidth -xshift-maxX,	// upright
+		ury = 2 -yshift-maxY,
+		dlx = 2 -xshift-maxX,			// downleft
+		dly = wrdHeight -yshift-maxY,
+		drx = wrdWidth -xshift-maxX,	// downright
+		dry = wrdHeight -yshift-maxY;
+	context.beginPath();
+	context.moveTo(ulx,uly);
+	context.lineTo(dlx,dly);
+	context.stroke();
+	context.beginPath();
+	context.moveTo(ulx,uly);
+	context.lineTo(urx,ury);
+	context.stroke();
+	context.beginPath();
+	context.moveTo(urx,ury);
+	context.lineTo(drx,dry);
+	context.stroke();
+	context.beginPath();
+	context.moveTo(drx,dry);
+	context.lineTo(dlx,dly);
+	context.stroke();
+}	// end drawGrid();
+
+function generateBlobs() {
+	var world_area = wrdWidth*wrdHeight;
+	var available_area = world_area;// - size;
+	/* blob_count * blobSize / available_area = density  */
+	var blob_count = (blobDensity * available_area) / blobSize;
+	//console.log(blob_count);
+	blobs = [];
+	for (var i = 0; i < blob_count; i++) {
+		//var pos_mag = size + 10 - (wrdWidth/2) + Math.random()*wrdWidth ;  
+		//var pos_angle = 2*Math.PI* Math.random();
+		//blobs.push(  new Point(pos_mag*Math.cos(pos_angle), pos_mag*Math.sin(pos_angle), colors[Math.floor((Math.random() * colors.length))])  );
+		blobs.push( new Point(-(wrdWidth/2) + wrdWidth*Math.random(), -(wrdHeight/2) + wrdHeight*Math.random(), colors[Math.floor((Math.random() * colors.length))]) );
+		blobs[i].isVirus = Math.floor(Math.random()*1000)%25 == 1.0;	// make some blobs viruses
+	}
+}
+
+function drawBlobs() {
+	for (var i = 0; i < blobs.length; i++) {
+		if(!blobs[i].isVirus)
+		drawCircle(blobs[i].x - xshift, blobs[i].y - yshift,
+				blobSize, 6, blobs[i].color, blobs[i].ang);
+		else
+		drawCircle(blobs[i].x - xshift, blobs[i].y - yshift,
+				blobSize*6, 17, "Chartreuse", 0);
+	}
+}
+
+// draw a polygon with given sides and radius(for the circumscribing circle) 
+function drawCircle(x,y,rad,sides,col,start) {
+	var ang = 2*Math.PI/sides;
+	var cur = start;
+
+	context.beginPath();
+	context.moveTo(x+ rad*Math.cos(cur), y+rad*Math.sin(cur));
+	for(var i=0; i<sides; i++) {
+		context.lineTo(x+ rad*Math.cos(cur+ang), y+rad*Math.sin(cur+ang));
+		cur += ang;
+	}	
+	context.closePath();
+	context.fillStyle = col;	
+	context.fill();
+}
+
+function initFPS() {
+	for(var i=0; i < 10; i++) fps.push(0.0);
+}
+
+function displayFPS(now){
+	function calAVG() {
+		   var count=0.0;
+		   for (var i=fps.length; i--;) 
+		     count+=fps[i];
+		   
+		   return Math.round(count/fps.length);
 	}
 
-	function drawBlobs() {
-		for (var i = 0; i < blobs.length; i++)
-			drawCircle(blobs[i].x - xshift, blobs[i].y - yshift,
-					blobSize, 6, blobs[i].color, blobs[i].ang);
-	}
+	fps[_ind_%10] = 1000.0/(now-lastTime);
+	_ind_++;
+	console.log(calAVG());
+}
 
-	function sizeToSpd(sz) {
+}	// end window.onload()
+
+/*************************************************************************************************************************/
+
+/* Helper Functions */
+
+function sizeToSpd(sz) {
 		return sz/10;
-	}
-
-
 }
 
 function distSq(x1,y1,x2,y2){
@@ -434,9 +458,9 @@ function distSq(x1,y1,x2,y2){
 
 
 function compUV(ux,uy,vx,vy){		// component of u along v
-	var udotu = ux*vx + uy*vy;
+	var udotv = ux*vx + uy*vy;
 	var vdotv = vx*vx + vy*vy;
-	var scaler = udotu/vdotv;
+	var scaler = udotv/vdotv;
 	return {
 		xcomp : vx*scaler,
 		ycomp : vy*scaler
